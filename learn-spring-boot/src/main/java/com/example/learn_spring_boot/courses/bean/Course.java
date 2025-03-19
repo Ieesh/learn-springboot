927608c540eb4f6daa4c51b63ad93a32fb357030
Hello <Hello@DESKTOP-I9LN8KN>
2025-03-19 17:32:18 +0530

    Initial commit

---
+++ learn-spring-boot/src/main/java/com/example/learn_spring_boot/courses/bean/Course.java
@@ -0,0 +1,41 @@
+package com.example.learn_spring_boot.courses.bean;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Course {
+
+	@Id
+	@GeneratedValue
+	private long id;
+
+	private String name;
+	private String author;
+
+	public Course() {
+
+	}
+
+	public Course(long id, String name, String author) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.author = author;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+
+}
---
+++ learn-spring-boot/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into COURSE(ID, AUTHOR, NAME)
+values (101,'in28 min', 'Learn Microservices');
+insert into COURSE(ID, AUTHOR, NAME)
+values (102,'in28 min', 'Learn Fullstack');
+insert into COURSE(ID, AUTHOR, NAME)
+values (103,'in28 min', 'Learn Azure');
+
